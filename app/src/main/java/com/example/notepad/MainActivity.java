3f087df0fb209262bb5cbbdb486c5706c0b81ce4
YuetaoZ <yuetao.zhu.062984@gmail.com>
2020-10-20 15:20:39 -0500

    updated MainActivity to show the note list via NoteAdapter.

--- app/src/main/java/com/example/notepad/MainActivity.java
+++ app/src/main/java/com/example/notepad/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.notepad;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
+    private final List<Note> noteList = new ArrayList<>();  // Main content is here
+    private RecyclerView recyclerView; // Layout's recyclerview
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recycler);
+        // Data to recyclerview adapter
+        NotesAdapter mAdapter = new NotesAdapter(noteList, this);
+        recyclerView.setAdapter(mAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //Make some data - not always needed - just used to fill list
+        for (int i = 0; i < 30; i++) {
+            noteList.add(new Note());
+        }
     }
 
     @Override
@@ -42,13 +65,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void addNewNote() {
     }
 
+    // From OnClickListener
     @Override
-    public void onClick(View v) {
+    public void onClick(View v) {  // click listener called by ViewHolder clicks
+        int pos = recyclerView.getChildLayoutPosition(v);
+        Note m = noteList.get(pos);
+        Toast.makeText(v.getContext(), "SHORT " + m.toString(), Toast.LENGTH_SHORT).show();
+    }
 
+    // From OnLongClickListener
+    @Override
+    public boolean onLongClick(View v) {  // long click listener called by ViewHolder long clicks
+        int pos = recyclerView.getChildLayoutPosition(v);
+        Note m = noteList.get(pos);
+        Toast.makeText(v.getContext(), "LONG " + m.toString(), Toast.LENGTH_SHORT).show();
+        return false;
     }
 
     @Override
-    public boolean onLongClick(View v) {
-        return false;
+    public void onBackPressed() {
+        Toast.makeText(this, "The back button was pressed - Bye!", Toast.LENGTH_SHORT).show();
+        super.onBackPressed();
     }
 }
